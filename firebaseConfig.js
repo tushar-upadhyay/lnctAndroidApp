13441795a8dfda8adc4e1e9d3c7d0c0126ce6b75
tusharrockpg <47355104+tusharrockpg@users.noreply.github.com>
2019-06-02 21:48:16 +0530

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-    apiKey: "AIzaSyCtTuEzXQuIxveURFAR8JPksH5jBvQEWtQ",
-    authDomain: "app-tokens.firebaseapp.com",
-    databaseURL: "https://app-tokens.firebaseio.com",
-    projectId: "app-tokens",
-    storageBucket: "app-tokens.appspot.com",
-    messagingSenderId: "1041757734412",
-    appId: "1:1041757734412:web:654b28dee531d83a"
-  };
